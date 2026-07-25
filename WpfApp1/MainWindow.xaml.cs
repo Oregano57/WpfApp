86b71cb93b79abb3f208457f3ca76af2c67a33d6
Oregano57 <tuberplazma@gmail.com>
2026-07-24 21:09:47 -0500

    Task Input Box height 20px from 30px AddButtonClick updates from _todoList

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -16,8 +16,47 @@ namespace WpfApp1;
 /// </summary>
 public partial class MainWindow : Window
 {
+    private ToDoApp app;
+    
     public MainWindow()
     {
         InitializeComponent();
+        app = new ToDoApp();
+    }
+
+    public class ToDoApp
+    {
+        private List<string> _todoList = new List<string>();
+
+        public void AddTask(string task)
+        {
+            _todoList.Add(task);
+        }
+
+        public List<string> Get()
+        {
+            return _todoList;
+        }
+
+        public void SetTasks(List<string> tasks)
+        {
+            _todoList = tasks;
+        }
+    }
+
+    public void RefreshTaskList()
+    {
+        TaskListBox.Items.Clear();
+        foreach (string task in app.Get())
+            TaskListBox.Items.Add(task);
+        
+    }
+
+    private void AddButton_Click(object sender, RoutedEventArgs e)
+    {
+        string task = TaskInput.Text;
+        app.AddTask(task);
+        RefreshTaskList();
+        TaskInput.Text = "";
     }
 }
