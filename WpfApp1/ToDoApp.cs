9fd5a22a3594468a26391d76f757d8bd57c2a9b0
Oregano57 <tuberplazma@gmail.com>
2026-07-25 23:25:06 -0400

    ToDoApp Class moved to its own file

---
+++ WpfApp1/ToDoApp.cs
@@ -0,0 +1,26 @@
+﻿namespace WpfApp1;
+
+public class ToDoApp
+{
+    private List<string> _todoList = new List<string>();
+
+    public void AddTask(string task)
+    {
+        _todoList.Add(task);
+    }
+
+    public List<string> Get()
+    {
+        return _todoList;
+    }
+
+    public void SetTasks(List<string> tasks)
+    {
+        _todoList = tasks;
+    }
+
+    public void RemoveTask(int index)
+    {
+        _todoList.RemoveAt(index);
+    }
+}
